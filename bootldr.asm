92cfa520116ae1cc621204fe8d99629dc7ac6790
mefjak000 <trzeciak.3mateusz@gmail.com>
2024-07-30 11:07:05 +0200

    Added string printing

--- bootldr.asm
+++ bootldr.asm
@@ -1,35 +1,38 @@
 BITS 16
 ORG 0x7C00
 
-global _main
+mov ax, 0
+mov ss, ax
+mov sp, 0x7C00
 
-section .data
-	boot_mess: db "Bootldr",0
-	boot_mess_len: equ  $-boot_mess
+call write_string
+hlt                                         ; halt CPU and hang here
 
-;section .bss
-
-section .text
-
-write_chr:
+; WRITE CHAR PROC
+write_char:
     mov al, [si]                            ; load character to al
     mov ah, 0x0E                            ; BIOS function - display character
     int 0x10                                ; call BIOS
     inc si                                  ; get next chr
-    ret
+    ret                                     ; Return
+; END
 
-_main: 
-    mov si, boot_mess                       ; si = &boot_mess
+; WRITE STRING PROC
+write_string: 
+xor cx, cx                                  ; cx = 0
+mov si, boot_mess                           ; si = &boot_mess
 
-print_next_chr: 
-    xor cx, cx                              ; cx as i = 0
-    call write_chr                          ; call subprocedure 
-    inc cx                                  ; i++
+print_next_char:
+    call write_char                         ; call subprocedure 
+    inc cx
     cmp cx, boot_mess_len                   ; if i < boot_mess_len
-    jl print_next_chr                       ; jump to @@
+    jl print_next_char                      ; jump 
+    ret
+; END
 
-    cli                                     ; disable interrupts
-    hlt                                     ; CPU halt
+; DATA
+boot_mess: db "Bootldr",0
+boot_mess_len: equ  $-boot_mess
 
-	times 510-($ - $$) db 0		            ; fill rest of the code
-	dw 0xAA55				                ; boot signature required by MBR
+times 510-($ - $$) db 0		                ; fill rest of the code
+dw 0xAA55				                    ; boot signature required by MBR
