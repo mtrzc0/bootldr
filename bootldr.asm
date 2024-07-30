2fa347796d38b6f9087dbf9c222e7e33e184cdb2
mefjak000 <trzeciak.3mateusz@gmail.com>
2024-07-30 09:54:01 +0200

    In .gitignore exluded .bin files; Added first bootloader functions

--- bootldr.asm
+++ bootldr.asm
@@ -1,17 +1,35 @@
 BITS 16
-ORG 0x7c00
+ORG 0x7C00
 
-global main
+global _main
 
 section .data
 	boot_mess: db "Bootldr",0
 	boot_mess_len: equ  $-boot_mess
 
-section .bss
+;section .bss
 
 section .text
 
-main: 
-	times 512-(boot_mess_len+16) db 0		; fill with zeros
-	dw 0xAA55				                ; boot signature required by MBR
+write_chr:
+    mov al, [si]                            ; load character to al
+    mov ah, 0x0E                            ; BIOS function - display character
+    int 0x10                                ; call BIOS
+    inc si                                  ; get next chr
+    ret
 
+_main: 
+    mov si, boot_mess                       ; si = &boot_mess
+
+print_next_chr: 
+    xor cx, cx                              ; cx as i = 0
+    call write_chr                          ; call subprocedure 
+    inc cx                                  ; i++
+    cmp cx, boot_mess_len                   ; if i < boot_mess_len
+    jl print_next_chr                       ; jump to @@
+
+    cli                                     ; disable interrupts
+    hlt                                     ; CPU halt
+
+	times 510-($ - $$) db 0		            ; fill rest of the code
+	dw 0xAA55				                ; boot signature required by MBR
